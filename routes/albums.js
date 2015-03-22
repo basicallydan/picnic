25fc8618c9c5dadb681a8610131624e89bc4312c
Dan Hough <daniel.hough@gmail.com>
2015-03-22 14:49:50 +0000

    Added sizes SCSS, made sign in form less crap

--- routes/albums.js
+++ routes/albums.js
@@ -109,7 +109,7 @@ router.get('/', auth({
 }), function(req, res, next) {
 		var ownershipCode = req.query.ownershipCode || req.cookies.ownershipCode;
 		var user = req.user;
-		if (!user && ownershipCode) {
+		if (user && ownershipCode) {
 			console.log('Looking for albums with ownershipCode', ownershipCode);
 			Album.findByOwnershipCode(ownershipCode, function(err, albums) {
 				var albumViewModels = _.map(albums, function(album) {
@@ -122,7 +122,7 @@ router.get('/', auth({
 					}
 				});
 			});
-		} else {
+		} else if (ownershipCode) {
 			console.log('Looking for albums with ownershipCode', ownershipCode);
 			Album.findByOwner(user, function(err, albums) {
 				console.log('Found', albums.length, 'albums owned by user', user.username);
@@ -135,6 +135,13 @@ router.get('/', auth({
 						web: '/a'
 					}
 				});
+			});
+		} else {
+			res.send({
+				albums:[],
+				links: {
+					web: '/a'
+				}
 			});
 		}
 	});
