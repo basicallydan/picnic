b60426a4c43d93a29e6f116db136fa4c294a3bfb
Dan Hough <daniel.hough@gmail.com>
2015-05-03 19:47:54 +0100

    Added imageview

---
+++ public/javascripts/src/views/ImageView.js
@@ -0,0 +1,23 @@
+import Dropzone from 'dropzone';
+import ZeroClipboard from 'zeroclipboard';
+import url from 'url';
+
+var AlbumView = Backbone.View.extend({
+	className:'image-view',
+	albumTemplate: require('../../../../views/image.handlebars'),
+	initialize: function() {
+		this.viewState = new Backbone.Model();
+		this.listenTo(this.viewState, 'change:loadedImage', this.updateRenderedImage);
+		// this.listenTo(this.model.album, 'sync', this.render);
+	},
+	render: function() {
+		let albumRendered = this.albumTemplate({
+			album: this.model.album.toJSON()
+		});
+	},
+	updateRenderedImage: function () {
+		let loadedImage = this.viewState.get('loadedImage');
+	}
+});
+
+export default AlbumView;
